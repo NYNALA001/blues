393852e25b23f26ab52002b2550516f72b617b4c
takundac <tkchikondo@gmail.com>
2017-08-21 17:43:37 +0200

    configured app settings.json

--- blues/Data/BusContext.cs
+++ blues/Data/BusContext.cs
@@ -1,22 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using blues.Models;
+
 namespace blues.Data
 {
     public class BusContext : DbContext
         {
         public BusContext(DbContextOptions<BusContext> options) : base(options)
             {
+
             }
             public DbSet<BusStop> BusStops { get; set; }
+
             public DbSet<GeoInfo> GeoInfos { get; set; }
+
             public DbSet<Route> Routes { get; set; }
+
             public DbSet<RouteBusStopTime> RouteBusStopTimes { get; set; }
+
             public DbSet<RouteSection> RouteSections { get; set; }
+
             public DbSet<TimeSheet> TimeSheets { get; set; }
     }
 }
